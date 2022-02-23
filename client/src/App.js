587e542b38c2da44923f81aa848e5ba0b98ecf9b
DhurbahariRanabhat <apple@Apple-no-MacBook-Air.local>
2022-02-23 23:59:32 +0530

    Dashboard Layout created

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.scss';
 import POS from './components/pos/pos';
 import { Routes, Route } from "react-router";
-import OrderPopup from './components/pos/extras/OrderPopup';
+import TopBar from './components/pos/extras/TopBar';
+import Dashboard from './components/dashboard/dashboard';
 
 
 function App() {
   return (
     <Routes>
       <Route index element = {<POS />} />
-      <Route path="/developing" element = {<OrderPopup show={true} onHide={() => {return true}}  />} />
+      <Route path="/developing" element = {<TopBar show={true} onHide={() => {return true}}  />} />
+      <Route path="/dashboard" element = {<Dashboard show={true} onHide={() => {return true}}  />} />
     </Routes>
   );
 }
