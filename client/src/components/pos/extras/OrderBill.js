da0e857114d2bd3a157dc5899c49ddc52d6bef80
NigamBhattarai <inigambhattarai@gmail.com>
2022-03-03 15:05:23 +0545

    updated: states connected with orderBill data and changed style

--- client/src/components/pos/extras/OrderBill.js
+++ client/src/components/pos/extras/OrderBill.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import CancelIcon from "@mui/icons-material/Cancel";
 import "./OrderBill.scss";
+import { POSContext } from "../pos";
 
 function getAbsoluteHeight(el) {
   el = typeof el === "string" ? document.querySelector(el) : el;
@@ -30,20 +31,32 @@ function OrderBill(props) {
     }
     window.addEventListener("resize", handleResize);
   });
-  var billItemsArray = props.billItems;
-  const [billItems, setBillItems] = useState(billItemsArray);
-  
+
+  const posContext = useContext(POSContext);
   function changeCountItem(i, action) {
-    let temp_state = [...billItems];
-    let temp_element = { ...temp_state[i] };
-    temp_element.count =
+    let temp_state = [...posContext.state.bill.billItems];
+    temp_state[i].count =
       action === "+"
-        ? (temp_element.count = temp_element.count + 1)
-        : temp_element.count-1 > 0
-        ? (temp_element.count = temp_element.count - 1)
+        ? (temp_state[i].count = temp_state[i].count + 1)
+        : temp_state[i].count-1 > 0
+        ? (temp_state[i].count = temp_state[i].count - 1)
         : 1;
-    temp_state[i] = temp_element;
-    setBillItems(temp_state);
+    posContext.dispatch({type:"updateBillItems",value:temp_state});
+    posContext.dispatch({ type: "updateBillPrices" });
+  }
+
+  function removeBillItem(i) {
+    let temp_state = [...posContext.state.bill.billItems];
+    temp_state.splice(i, 1);
+    posContext.dispatch({type:"updateBillItems",value:temp_state});
+    posContext.dispatch({ type: "updateBillPrices" });
+  }
+
+  function removeAddOnBillItem(i, si) {
+    let temp_state = [...posContext.state.bill.billItems];
+    temp_state[i].extras.splice(si, 1);
+    posContext.dispatch({type:"updateBillItems",value:temp_state});
+    posContext.dispatch({ type: "updateBillPrices" });
   }
   return (
     <div className="order-bill py-3">
@@ -55,9 +68,9 @@ function OrderBill(props) {
         </Row>
       </Container>
       <Container className="order-bill-items">
-        {billItems.map((value, i) => {
+        {posContext.state.bill.billItems.map((value, i) => {
           return (
-            <>
+            <div key={i+value.id.toString()+value.name}>
               <Row className="order-bill-item">
                 <Col xs={2}>
                   <img
@@ -92,13 +105,13 @@ function OrderBill(props) {
                   <span className="order-bill-item-price">
                     Rs. {value.price * value.count}
                   </span>
-                  <CancelIcon className="ml-2 order-bill-item-cancel" />
+                  <CancelIcon onClick={(e)=>{removeBillItem(i)}} className="ml-2 order-bill-item-cancel" />
                 </Col>
               </Row>
               <Container>
                 {value.extras.map((extra, subindex) => {
                   return (
-                    <Row className="order-bill-addon-row">
+                    <Row key={subindex+extra.id.toString()+extra.name} className="order-bill-addon-row">
                       <Col xs={9} className="order-bill-addon-name">
                         {"+" + extra.name}
                       </Col>
@@ -106,13 +119,13 @@ function OrderBill(props) {
                         <span className="order-bill-addon-price">
                           Rs. {extra.price}
                         </span>
-                        <CancelIcon className="ml-2 order-bill-addon-cancel" />
+                        <CancelIcon onClick={(e)=>{removeAddOnBillItem(i, subindex)}} className="ml-2 order-bill-addon-cancel" />
                       </Col>
                     </Row>
                   );
                 })}
               </Container>
-            </>
+            </div>
           );
         })}
       </Container>
@@ -122,13 +135,13 @@ function OrderBill(props) {
           <Col xs={5} className="ml-3">
             Sub Total
           </Col>
-          <Col xs={3}>Rs. 200</Col>
+          <Col xs={3}>Rs. {posContext.state.bill.prices.subTotal}</Col>
         </Row>
         <Row className="justify-content-between order-bill-total-box order-bill-tax">
           <Col xs={5} className="ml-3">
             Tax
           </Col>
-          <Col xs={3}>Rs. 200</Col>
+          <Col xs={3}>Rs. {posContext.state.bill.prices.tax}</Col>
         </Row>
         <Row className="order-bill-total-box order-bill-discount">
           <Col xs={1} className="ml-3">
@@ -136,26 +149,28 @@ function OrderBill(props) {
           </Col>
           <Col xs={2} className="ml-4 mr-auto">
             <Form.Control
-              type="number"
+              // type="number"
               placeholder="0%"
+              disabled={true}
+              value={posContext.state.bill.prices.discountPercentage+"%"}
               className="order-bill-discount-percentage"
               max="100"
             />
           </Col>
-          <Col xs={3}>Rs. 200</Col>
+          <Col xs={3}>Rs. {posContext.state.bill.prices.discountValue}</Col>
         </Row>
         <Row className="justify-content-between order-bill-total-box order-bill-grand-total">
           <Col xs={5} className="ml-3">
             Total
           </Col>
           <Col xs={3} className="ml-auto">
-            Rs. 200
+          Rs. {posContext.state.bill.prices.total}
           </Col>
         </Row>
       </Container>
       <Container>
         <Row className="justify-content-center">
-          <Button variant="light" className="order-bill-place-order-btn">
+          <Button variant="light" className="default-button order-bill-place-order-btn">
             Place Order
           </Button>
         </Row>
