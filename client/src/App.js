aaf7e7f28d702f91d153f24f2094bdf8e8189af0
anmol <anamolpradhan@gmail.com>
2022-03-06 11:52:13 +0530

    title and orderlist report created in FullReport

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import POS from './components/pos/pos';
 import { Routes, Route } from "react-router";
 import TopBar from './components/pos/extras/TopBar';
-import Main from './components/dashboard/Main';
+import FullReport from './components/dashboard/FullReport';
 import Dashboard from './components/dashboard/dashboard';
 
 
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route index element = {<POS />} />
       <Route path="/developing" element = {<TopBar show={true} onHide={() => {return true}}  />} />
-      <Route path="/dashboard" element = {<Dashboard><Main show={true} onHide={() => {return true}}  /></Dashboard>} />
+      <Route path="/dashboard" element = {<Dashboard><FullReport show={true} onHide={() => {return true}}  /></Dashboard>} />
     </Routes>
   );
 }
