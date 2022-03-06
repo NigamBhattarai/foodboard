21242c7589d64c2eb57e907817376005b1f4728a
anmol <anamolpradhan@gmail.com>
2022-03-06 11:55:10 +0530

    title and orderlist report created in FullReport

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,14 @@ import POS from './components/pos/pos';
 import { Routes, Route } from "react-router";
 import TopBar from './components/pos/extras/TopBar';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Categories from './components/dashboard/Categories';
 import Order from '../src/components/dashboard/Order';
 =======
 import Main from './components/dashboard/Main';
+=======
+import FullReport from './components/dashboard/FullReport';
+>>>>>>> aaf7e7f (title and orderlist report created in FullReport)
 import Dashboard from './components/dashboard/dashboard';
 >>>>>>> 22f40e3ef49e8a344a195a06ecac302c36c0f807
 
@@ -17,11 +21,15 @@ function App() {
       <Route index element = {<POS />} />
       <Route path="/developing" element = {<TopBar show={true} onHide={() => {return true}}  />} />
 <<<<<<< HEAD
+<<<<<<< HEAD
       <Route path="/dashboard" element = {<Categories show={true} onHide={() => {return true}}  />} />
       <Route path='/orderpage'element={<Order />} />
 =======
       <Route path="/dashboard" element = {<Dashboard><Main show={true} onHide={() => {return true}}  /></Dashboard>} />
 >>>>>>> 22f40e3ef49e8a344a195a06ecac302c36c0f807
+=======
+      <Route path="/dashboard" element = {<Dashboard><FullReport show={true} onHide={() => {return true}}  /></Dashboard>} />
+>>>>>>> aaf7e7f (title and orderlist report created in FullReport)
     </Routes>
   );
 }
--- client/src/components/dashboard/FullReport.js
+++ client/src/components/dashboard/FullReport.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from "react";
+import {
+  Row,
+  Col,
+  Container,
+  Button,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
+
+import OrderListTable from "../pos/extras/OrderListTable.js";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import "./FullReport.scss"
 
 function FullReport(props) {
   return (
-    <div>FullReport</div>
-  )
+    <Container fluid>
+      <Row className="title align-items-center">
+        <Col lg={3}>
+          <h2>Full Report</h2>
+          <small className="text-muted">Tuesday 2,Feb,2021</small>
+        </Col>
+        <Col></Col>
+        <Col lg={1}>
+          <DropdownButton
+            id="dropdown-basic-button"
+            variant="light"
+            className="button1"
+            title="Today"
+          >
+            <Dropdown.Item href="#/action-1">Yesterday</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Last 7 days</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">This Month</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+      </Row>
+      <hr />
+      <Col lg={7} className="rounded order-report py-3">
+        <Row className="mx-0">
+          <Col lg={4}>
+            <h4>Orders</h4>
+          </Col>
+          <Col></Col>
+          <Col lg={3}>
+            <Button variant="light" className="button1">
+              <ListAltIcon className="mr-2" />
+              Full List
+            </Button>{" "}
+          </Col>
+        </Row>
+        <OrderListTable />
+      </Col>
+    </Container>
+  );
 }
 
-
-export default FullReport
+export default FullReport;
