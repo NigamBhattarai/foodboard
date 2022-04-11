24c8aa292f69c32a30b8ff89979bd24e0e30ea94
anmol <anamolpradhan@gmail.com>
2022-04-11 17:05:30 +0530

    loader and error messgae displayed to OrderReport

--- client/src/components/dashboard/OrderReport.js
+++ client/src/components/dashboard/OrderReport.js
@@ -1,10 +1,12 @@
-import React, { useState,useEffect,useReducer } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { Row, Col, Container, Button, Table, Badge } from "react-bootstrap";
 import AddBusinessIcon from "@mui/icons-material/AddBusiness";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import axios from "axios";
-import PrintIcon from '@mui/icons-material/Print';
+import PrintIcon from "@mui/icons-material/Print";
 
+import LoadingBox from "./components/LoadingBox";
+import MessageBox from "./components/MessageBox";
 function reducer(state, action) {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -37,7 +39,6 @@ function OrderReport() {
     };
     fetchData();
   }, []);
-  const [showModal, setShowModal] = useState(false);
   return (
     <Container fluid className="mx-2 categories">
       <Row className="title align-items-center mx-0">
@@ -53,53 +54,64 @@ function OrderReport() {
 
         <Col lg={2}>
           <Button variant="light" className="titleButton">
-            <AddBusinessIcon/>
+            <AddBusinessIcon />
             Manage Orders
           </Button>{" "}
         </Col>
       </Row>
       <hr />
-
-      <Row>
-        <Col md="12">
-          <Table className="table-borderless align-middle"Visibility hover size="sm">
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Token</th>
-                <th>No. Of Items</th>
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                orders.map((order)=>{
-                  const classname="pill-"+order.status;
-                  return (<tr>
-                <td>{order.ordered_time}</td>
-                <td>12:40</td>
-                <td>{order.token_number}</td>
-                <td>3</td>
-                <td>
-                  <Badge pill bg="primary" className={classname}>
-                    {order.status}
-                  </Badge>
-                </td>
-                <td>
-                  <VisibilityIcon
-                    className="mr-2 icon"
-                  ></VisibilityIcon>
-                  <PrintIcon
-                    className="mr-2 icon"
-                  ></PrintIcon>
-                </td>
-              </tr>)})}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
+      <div className="main-body">
+      {loading ? (
+          <center>
+            <LoadingBox />
+          </center>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+        <Row>
+          <Col md="12">
+            <Table
+              className="table-borderless align-middle"
+              Visibility
+              hover
+              size="sm"
+            >
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Token</th>
+                  <th>No. Of Items</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((order) => {
+                  const classname = "pill-" + order.status;
+                  return (
+                    <tr>
+                      <td>{order.ordered_time}</td>
+                      <td>12:40</td>
+                      <td>{order.token_number}</td>
+                      <td>3</td>
+                      <td>
+                        <Badge pill bg="primary" className={classname}>
+                          {order.status}
+                        </Badge>
+                      </td>
+                      <td>
+                        <VisibilityIcon className="mr-2 icon"></VisibilityIcon>
+                        <PrintIcon className="mr-2 icon"></PrintIcon>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>)}
+      </div>
     </Container>
   );
 }
