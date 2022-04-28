126e4344a8eb49db054b059924c58a09b16be303
NigamBhattarai <inigambhattarai@gmail.com>
2022-04-28 12:43:30 +0545

    logout option added in topbar; new topbar(header) created for when user is not logged in

---
+++ client/src/components/outside/fragments/OutsideHeader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function OutsideHeader() {
+  return (
+    <Container fluid>
+      <Row style={{ backgroundColor: "#fff", padding: "5px 0px" }}>
+        <Col xs={6} sm={4} md={4} lg={2}>
+          <img
+            src="/images/foodboard-logo.png"
+            className="img-fluid"
+            alt="logo"
+          />
+        </Col>
+        <Col xs={6} sm={4} md={4} lg={2} className="ml-auto my-auto">
+          <Link to={"/signin"} className="mr-4">Sign in</Link>
+          <Link to={"/signup"}>Register</Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
