17f3e4dbab742471dc0e12ddf24c99f523ff7c90
NigamBhattarai <inigambhattarai@gmail.com>
2022-05-17 21:26:55 +0545

    added: selected attribute to addons in food.controller; added: default field in variant for false values too in seed and variant.model

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -1,24 +1,34 @@
 const foodModel = require("../models/food.model");
 const variantModel = require("../models/variant.model");
+const addOnModel = require("../models/addon.model");
 require("../models/category.model");
 
 exports.getAllFoods = (req, res) => {
-    foodModel
-      .find({})
-      .lean()
-      .populate("category")
-      .exec(async (err, result) => {
-        if (result) {
-            for (var i = 0; i < result.length; i++) {
-              result[i].variants = await variantModel
-                .find({ food: result[i]._id })
-                .exec();
+  foodModel
+    .find({})
+    .lean()
+    .populate("category")
+    .exec(async (err, result) => {
+      if (result) {
+        for (var i = 0; i < result.length; i++) {
+          result[i].variants = await variantModel
+            .find({ food: result[i]._id })
+            .lean()
+            .exec();
+          for (var j = 0; j < result[i].variants.length; j++) {
+            result[i].variants[j].addons = await addOnModel
+              .find({ variant: result[i].variants[j]._id })
+              .lean()
+              .exec();
+            for (var k = 0; k < result[i].variants[j].addons.length; k++) {
+              result[i].variants[j].addons[k].selected = false;
             }
-            res.send(result);
-          } else {
-            console.log(err);
-            res.send(err);
           }
-          });
-  };
-  
+        }
+        res.send(result);
+      } else {
+        console.log(err);
+        res.send(err);
+      }
+    });
+};
