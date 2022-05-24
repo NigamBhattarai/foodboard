4940b75c9a7e19f01ba2621ad8a3fa5f25865146
NigamBhattarai <inigambhattarai@gmail.com>
2022-05-24 15:43:35 +0545

    removed: scrollbar from dashboard; Order page completed; OrderReport page completed except view and print

--- client/src/components/dashboard/OrderReport.js
+++ client/src/components/dashboard/OrderReport.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Row, Col, Container, Button, Table, Badge } from "react-bootstrap";
-import AddBusinessIcon from "@mui/icons-material/AddBusiness";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import axios from "axios";
 import PrintIcon from "@mui/icons-material/Print";
-
+import dateFormat from "dateformat";
+import { io } from "socket.io-client";
 import LoadingBox from "./components/LoadingBox";
 import MessageBox from "./components/MessageBox";
 import UseTitle from "../../hooks/useTitle";
+import { useAlert } from "react-alert";
+
 function reducer(state, action) {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -20,17 +22,71 @@ function reducer(state, action) {
       return state;
   }
 }
+function renderStatus(order) {
+  switch (order.status) {
+    case 0:
+      return (
+        <Badge pill className="pill-pending">
+          Pending
+        </Badge>
+      );
+    case 1:
+      return (
+        <Badge pill className="pill-preparing">
+          Preparing
+        </Badge>
+      );
+    case 2:
+      return (
+        <Badge pill className="pill-served">
+          Served
+        </Badge>
+      );
+    case 3:
+      return (
+        <Badge pill className="pill-canceled">
+          Canceled
+        </Badge>
+      );
+    case 4:
+      return (
+        <Button
+          variant="outline-success"
+          style={{ width: 100 }}
+          onClick={() => submitData(2, order._id)}
+        >
+          Serve
+        </Button>
+      );
+    default:
+      break;
+  }
+}
+function submitData(status, id) {
+  axios.post(process.env.REACT_APP_API_URL + "api/orders/updateStatus", {
+    status: status,
+    id: id,
+  });
+  alert.success("Order has been served");
+
+}
 
 function OrderReport() {
   UseTitle("Order Report");
+  const [nullState] = useState();
   const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
     orders: [],
     loading: true,
     error: "",
   });
+  const alert = useAlert();
 
-  useEffect(() => {
-    const fetchData = async () => {
+  function convertTime(time) {
+    return dateFormat(time, "hh:mm TT").toString();
+  }
+
+  async function fetchData() {
+    {
       dispatch({ type: "FETCH_REQUEST" });
       try {
         const result = await axios.get(
@@ -40,27 +96,27 @@ function OrderReport() {
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
-    };
+    }
+  }
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+    socket.on("newOrder", () => {
+      fetchData();
+    });
+    socket.on("updateOrderStatus", () => {
+      fetchData();
+
+    });
     fetchData();
-  }, []);
+  }, [nullState]);
   return (
-    <Container fluid className="mx-2 categories">
+    <Container fluid className=" categories mt-4">
       <Row className="title align-items-center mx-0">
         <Col lg={2}>
           <Row>
             {" "}
-            <h2>Order Table</h2>
-            <small className="text-muted">Tuesday 2,Feb,2021</small>{" "}
+            <h2>Orders</h2>
           </Row>
-        </Col>
-
-        <Col> </Col>
-
-        <Col lg={2}>
-          <Button variant="light" className="titleButton">
-            <AddBusinessIcon />
-            Manage Orders
-          </Button>{" "}
         </Col>
       </Row>
       <hr />
@@ -72,49 +128,66 @@ function OrderReport() {
         ) : error ? (
           <MessageBox variant="danger">{error}</MessageBox>
         ) : (
-          <Row>
-            <Col md="12">
-              <Table
-                className="table-borderless align-middle"
-                Visibility
-                hover
-                size="sm"
-              >
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Token</th>
-                    <th>No. Of Items</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((order, index) => {
-                    const classname = "pill-" + order.status;
-                    return (
-                      <tr key={"order-" + index}>
-                        <td>{order.ordered_time}</td>
-                        <td>12:40</td>
-                        <td>{order.token_number}</td>
-                        <td>3</td>
-                        <td>
-                          <Badge pill bg="primary" className={classname}>
-                            {order.status}
-                          </Badge>
-                        </td>
-                        <td>
-                          <VisibilityIcon className="mr-2 icon"></VisibilityIcon>
-                          <PrintIcon className="mr-2 icon"></PrintIcon>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
+          <Container fluid>
+            <Row>
+              <Col md="12">
+                <Table
+                  className="table-borderless align-middle"
+                  hover
+                  size="sm"
+                >
+                  <thead>
+                    <tr>
+                      <th>Customer Name</th>
+                      <th>Time</th>
+                      <th>Token</th>
+                      <th>No. Of Items</th>
+                      <th>Items</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orders
+                      .filter(
+                        (order) =>
+                          order.status === 0 ||
+                          order.status === 1 ||
+                          order.status === 4
+                      )
+                      .map((order, index) => {
+                        const classname = "pill-" + order.status;
+                        return (
+                          <tr key={"order-" + index}>
+                            <td>{order.customer_name}</td>
+                            <td>{convertTime(order.ordered_time)}</td>
+                            <td>{order.token_number}</td>
+                            <td>{order.foodItems.length}</td>
+                            <td>
+                              {order.foodItems
+                                .map((foodItem) => {
+                                  return (
+                                    foodItem.variant.name +
+                                    " " +
+                                    foodItem.variant.food.name
+                                  );
+                                })
+                                .toString()
+                                .substring(0, 40)}
+                            </td>
+                            <td>{renderStatus(order)}</td>
+                            <td>
+                              <VisibilityIcon className="mr-2 icon"></VisibilityIcon>
+                              <PrintIcon className="mr-2 icon"></PrintIcon>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Container>
         )}
       </div>
     </Container>
